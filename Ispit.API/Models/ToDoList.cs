8630b03b192f2d99caa3150ccc511ca434895f9a
Danijel Pobi <121439317+theDanijel666@users.noreply.github.com>
2024-07-08 19:29:23 +0200

    Add project files.

---
+++ Ispit.API/Models/ToDoList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ispit.API.Models;
+
+public partial class ToDoList
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool? IsCompleted { get; set; }
+}
---
+++ Ispit.API/Models/ToDoWebApiContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.API.Models;
+
+public partial class ToDoWebApiContext : DbContext
+{
+    public ToDoWebApiContext()
+    {
+    }
+
+    public ToDoWebApiContext(DbContextOptions<ToDoWebApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ToDoList> ToDoLists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ToDoBaza");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ToDoList>(entity =>
+        {
+            entity.ToTable("ToDoList");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Description).HasMaxLength(2000);
+            entity.Property(e => e.Title).HasMaxLength(200);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
